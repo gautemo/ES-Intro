9d5b27cd281d1e6c3896b42ec20031e0769bc9a5
Gaute Meek Olsen <gaute.meek@gmail.com>
2019-05-06 23:11:57 +0200

    refactor

--- es9/promise-finally.js
+++ es9/promise-finally.js
@@ -1,8 +1,9 @@
 new Promise((resolve, reject) => {
-    setTimeout(() => reject(new Error('hi')), 300); //If value should be retured, do it inside resolve()
+    setTimeout(() => reject(new Error('hi')), 300);
+    //throw new Error('nooo');
 })
     .then(data => console.log(data))
     .catch(error => console.log(error))
     .finally(() => console.log('finished'));
 
-//es9 allows you to use finally to run code after a promise, regardless if it's resolved or rejected
+//es9 allows you to use finally to run code after a promise, regardless if it's resolved, rejected or crashed
