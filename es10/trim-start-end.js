e40168af0d4f8e3c73adb888acf861a67f833428
Gaute Meek Olsen <gaute.meek@gmail.com>
2019-08-26 21:05:58 +0200

    trim start and end for es10 folder

---
+++ es10/trim-start-end.js
@@ -0,0 +1,5 @@
+//ES10 has trimStart and trimEnd for strings
+
+const msg = '   Hello people and wolves   '
+console.log(msg.trimStart()); //Output: 'Hello people and wolves   '
+console.log(msg.trimEnd()); //Output: '   Hello people and wolves'
